f39955f11b30f467af4f364a1ab96d4e3909af6d
Fabien Cortez <114923959+fabct@users.noreply.github.com>
2024-05-27 08:58:46 +0200

    .

---
+++ src/main/java/com/projet_paris_sportif/ProjetParisSportifApplication.java
@@ -0,0 +1,13 @@
+package com.projet_paris_sportif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetParisSportifApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetParisSportifApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projet_paris_sportif/ProjetParisSportifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet_paris_sportif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetParisSportifApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
